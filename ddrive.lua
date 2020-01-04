0c9a20328718321efe4a6e544e5474c4369b288e
inffire <inffirepro@gmail.com>
2020-01-04 23:05:03 +0700

    fix wrong Rancher behaviour with improved getVehicleSubClass()

--- ddrive.lua
+++ ddrive.lua
@@ -1,11 +1,26 @@
 script_name('ddrive')
 script_description('kill the engine dec')
 script_moonloader(018)
-script_version_number(1)
+script_version_number(11)
 script_author('inf')
 
 local mem = require "memory"
 local Vector3D = require "vector3d"
+
+local eVehicleType = {
+    VEHICLE_AUTOMOBILE = 0,
+    VEHICLE_MTRUCK = 1,
+    VEHICLE_QUAD = 2,
+    VEHICLE_HELI = 3,
+    VEHICLE_PLANE = 4,
+    VEHICLE_BOAT = 5,
+    VEHICLE_TRAIN = 6,
+    VEHICLE_FHELI = 7,
+    VEHICLE_FPLANE = 8,
+    VEHICLE_BIKE = 9,
+    VEHICLE_BMX = 10,
+    VEHICLE_TRAILER = 11
+}
 
 local NARDCORE_MODE = false
 
@@ -24,6 +39,13 @@ function isVehicleMovingForward(vehicle)
 	return true
 end
 
+function getVehicleSubClass(vehicle)
+	local subclass = 0
+	local pCar = getCarPointer(vehicle)
+	if pCar then subclass = mem.getuint32(pCar + 0x594) end
+	return subclass
+end
+
 function main()
 	repeat wait(100) until isSampAvailable()
 
@@ -35,15 +57,20 @@ function main()
 		if isCharInAnyCar(playerPed) then
 			car = storeCarCharIsInNoSave(playerPed)
 		end
-		if car and getDriverOfCar(car) == playerPed and getCarSpeed(car) > stickBelow and
-			getVehicleClass(car) >= 0 and getVehicleClass(car) ~= 11
-		then
-			local a, b = getPadState(0, 16), getPadState(0, 14)
-			if a < 1 and b < 1 then
-				if isVehicleMovingForward(car) then
-					setGameKeyState(16, 3)
-				else
-					setGameKeyState(14, 3)
+		if car and getDriverOfCar(car) == playerPed and getCarSpeed(car) > stickBelow then
+			local subclass = getVehicleSubClass(car)
+			if subclass ~= eVehicleType.VEHICLE_HELI and
+				subclass ~= eVehicleType.VEHICLE_PLANE and
+				subclass ~= eVehicleType.VEHICLE_BOAT and
+				subclass ~= eVehicleType.VEHICLE_BMX
+			then
+				local a, b = getPadState(0, 16), getPadState(0, 14)
+				if a < 1 and b < 1 then
+					if isVehicleMovingForward(car) then
+						setGameKeyState(16, 3)
+					else
+						setGameKeyState(14, 3)
+					end
 				end
 			end
 		end
